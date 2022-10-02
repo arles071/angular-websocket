0eb8b93b1ba3d298a431d4df7512acaf3fd4453f
Fredy Yela <fredy.yela@edwcorp.com>
2022-10-01 21:22:49 -0500

    iniciando en websocket en angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import Echo from 'laravel-echo';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'frontsimple';
+export class AppComponent implements OnInit {
+  title = '';
+
+  ngOnInit() {
+    this.webSockets();
+  }
+
+  webSockets() {
+      const echo = new Echo({
+        broadcaster: 'pusher',
+        cluster: 'mt1',
+        key: 'ASD1234FG',
+        wsHost: window.location.hostname,
+        wsPort: 6001,
+        forceTLS: false,
+        disableStats: true,
+        enabledTransports: ['ws']
+    })
+
+    echo.channel('channel-message').listen('MessageEvent', (resp: any) => {
+      console.log(resp);
+      this.title = resp.message;
+    });
+  }
 }
